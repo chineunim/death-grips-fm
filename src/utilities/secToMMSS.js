b444a1532dac30bb2b1d1eff43917a9685260798
chineunim <ilyankshn@gmail.com>
2024-04-12 14:05:06 +0300

    delete moment package

--- src/utilities/secToMMSS.js
+++ src/utilities/secToMMSS.js
@@ -1,3 +1,7 @@
-import moment from "moment";
-
-export default (sec) => moment.utc(sec * 1000).format("mm:ss");
+export default (sec) => {
+	const minutes = Math.floor(sec / 60);
+	const seconds = sec % 60;
+	const formattedMinutes = String(minutes).padStart(2, "0");
+	const formattedSeconds = String(seconds).padStart(2, "0");
+	return `${formattedMinutes}:${formattedSeconds}`;
+  };
